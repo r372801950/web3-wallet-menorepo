7c26a22aac30995caf2387a866ac300db2fe9a47
JiangSir <chong.jiang@transwarp.io>
2025-02-19 01:37:41 +0800

    初始化web3-react

---
+++ packages/YD-web3-react/src/components/connectionsCards/MetaMaskCard.tsx
@@ -0,0 +1,13 @@
+import {hooks} from "@connections/metaMask";
+
+const {
+  useChainId,
+  useAccounts,
+  useIsActivating,
+  useIsActive,
+  useProvider,
+  useENSNames
+} = hooks;
+
+export default function MetaMaskCard() {
+};
